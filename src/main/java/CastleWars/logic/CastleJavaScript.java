8cd292144e0282220cc9ad2b59fc3db2b5d688bc
catsz <36317213+catsz@users.noreply.github.com>
2021-03-29 23:40:33 -0500

    add init

--- src/main/java/CastleWars/logic/CastleJavaScript.java
+++ src/main/java/CastleWars/logic/CastleJavaScript.java
@@ -12,10 +12,17 @@ be added inside javascript.
 */
 public class CastleJavaScript {
   public final logic;
+  private boolean set = false;
   
   public CastleJavaScript(Logic logic) {
     this.logic = logic;
     ScriptableObject.putConstProperty(Reflect.get(Vars.mods.getScripts(), "scope"), "CW", this);
+  }
+  
+  public static void init(Logic logic) {
+    if (set) return;
+    set = true;
+    new CastleJavaScript(logic);
   }
   
   public void giveMoney(int amount) {
